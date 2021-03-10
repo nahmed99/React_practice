8f482b8344f05a09d0ce299577048daca0b7f9e5
nahmed99 <ahmed_nawaz@hotmail.com>
2021-03-10 21:43:25 +0000

    Some code added to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
 
+   // The following variables are 'reactove', i.e., they are on a watchlist for any changes.
+   // If changes are detected, then the app will react to those changes (re-render the template where they exist)
+   const [name, setName] = useState("Yaar");
+
    const handleClick = () => {
-      console.log("Kidda");
+      setName("Oi!");
    }
 
    return (
       <div className="home">
          <h2>Homepage</h2>
+         <p> { name } </p>
          <button onClick={handleClick} >Click Me!</button>
       </div>
    );
