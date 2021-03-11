9050f54581e6565599d4b31849fcb09dc318dbdc
nahmed99 <ahmed_nawaz@hotmail.com>
2021-03-11 22:10:30 +0000

    Props structured and destructured

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+
+//const BlogList = ( props ) => {  // Note: No curly braces
+   // blogs = props.blogs;   // This way you have to retrieve from the props high-level qualifier
+   // title = props.title;
+
+const BlogList = ( { blogs, title } ) => {  // De-structured variable(s)
+
+   return (
+      <div className="blog-list">
+         <h2> {title} </h2>
+         { blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+               <h2> { blog.title } </h2>
+               <p>Written by { blog.author } </p>
+            </div>
+         )) }
+      </div>
+   );
+
+}
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
    // The following variables are 'reactove', i.e., they are on a watchlist for any changes.
    // If changes are detected, then the app will react to those changes (re-render the template where they exist)
-   const [name, setName] = useState("Yaar");
+   const [blogs, setBlogs] = useState([
+      { title: "The State", body: "Control the population", author: "Me Oh My", id: 1},
+      { title: "The Republic", body: "Exploit the population", author: "What You Too", id: 2},
+      { title: "The Monarchy", body: "Rule the population", author: "Do Not Ask", id: 3}
+   ]);
 
-   const handleClick = () => {
-      setName("Oi!");
-   }
+   
 
    return (
       <div className="home">
-         <h2>Homepage</h2>
-         <p> { name } </p>
-         <button onClick={handleClick} >Click Me!</button>
+         <BlogList blogs={ blogs } title="All Blogs!" />
       </div>
    );
 }
