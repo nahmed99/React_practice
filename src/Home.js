691ec8199c5000e7cb2fc4bd7b1a8b2d1f5bcd06
nahmed99 <ahmed_nawaz@hotmail.com>
2021-03-14 18:59:11 +0000

    Added data/db.json for json server to pick up. Invoke json server: npx json-server --watch data/db.json --port 8000

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,17 @@ const Home = () => {
 
    // The following variables are 'reactove', i.e., they are on a watchlist for any changes.
    // If changes are detected, then the app will react to those changes (re-render the template where they exist)
-   const [blogs, setBlogs] = useState([
-      { title: "The State", body: "Control the population", author: "Me Oh My", id: 1},
-      { title: "The Republic", body: "Exploit the population", author: "What You Too", id: 2},
-      { title: "The Monarchy", body: "Rule the population", author: "Do Not Ask", id: 3},
-      { title: "None Of The Above", body: "I Am", author: "Free!", id: 4}
-   ]);
+
+   const [blogs, setBlogs] = useState(null);
+
+   // const [blogs, setBlogs] = useState([
+   //    { title: "The State", body: "Control the population", author: "Me Oh My", id: 1},
+   //    { title: "The Republic", body: "Exploit the population", author: "What You Too", id: 2},
+   //    { title: "The Monarchy", body: "Rule the population", author: "Do Not Ask", id: 3},
+   //    { title: "None Of The Above", body: "I Am", author: "Free!", id: 4}
+   // ]);
+
+   
 
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -19,19 +24,25 @@ const Home = () => {
    }
 
    
-   // useEffect (without parameters in an array at the end) will run everytime the page is rendered.
+   // useEffect (without a dependency array) will run everytime the page is rendered.
    useEffect(() => {
-      console.log('useEffect ran');
-      console.log(blogs);
-   });
+      fetch('http://localhost:8000/blogs')
+      .then(response => {
+         return response.json() //parses json data into JS object format - in this case, that is an array.
+      })
+      .then((data) => { // date returned from above (then)
+         setBlogs(data);
+         console.log(data);
+      })
+   }, []);  // empty dependency array ensures that useEffect only runs on initial render, and not for any subsequent renders.
 
 
    return (
       <div className="home">
-         <BlogList blogs={ blogs } 
+         {blogs && <BlogList blogs={ blogs } 
                    title="All Blogs!" 
                    handleDelete={handleDelete} 
-                   />
+                   />}
          {/* <BlogList blogs={ blogs.filter((blog) => blog.author==="Free!") } title="Free's Blogs!" /> */}
       </div>
    );
